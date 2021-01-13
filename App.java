9af8a8a9b6174d664d0107720543ac43adc5fffd
Holdin0 <75454065+Holdin0@users.noreply.github.com>
2021-01-13 11:27:47 -0500

    Added hello, word list

--- App.java
+++ App.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class App {
+	
+	public static String[] words = {"Banana" "Apple" "Orange" "Mango" "Peach" "Plum" "Strawberries" "Kiwi" "Cherry"};
+	
     public static void main(String[] args){
+    	System.out.println("Hello!");
+    	
     }
 }
