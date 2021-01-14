5ab0eb11c95fe40f5e975d9cea9dc94e32aaf85b
Holden Ickovics <holden.ickovics@mynbps.org>
2021-01-14 16:21:21 +0000

    Added Comments and three lines of code (String and Print Statements)

--- App.java
+++ App.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class App {
+
+public static void main (String []   args) {
+		String[] words = {"Banana", "Apple", "Orange", "Mango","Peach","Plum", "Strawberries", "Kiwi","Cherry"}; //Guessed Words
+		String word = words [(int) (Math.random() * words.length)];
+		System.out.println("Welcome, Lets Play Hangman!");
+		System.out.println("Guess A Letter!");
+		//User types in a letter
+		
+		//Check if letter exists within the word
+		
+		//If correct, print letter within the now resolved area
+		
+		//If incorrect, print "Incorrect"
+		
+		
 	
-	public static String[] words = {"Banana" "Apple" "Orange" "Mango" "Peach" "Plum" "Strawberries" "Kiwi" "Cherry"};
-	
-    public static void main(String[] args){
-    	System.out.println("Hello!");
-    	
+    
     }
 }
