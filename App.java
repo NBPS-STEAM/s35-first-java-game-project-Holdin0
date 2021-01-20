c9ef8130e3013c30c219855d2e8ca35b8d1a973e
Holden Ickovics <holden.ickovics@mynbps.org>
2021-01-20 04:55:25 +0000

    Added Comments throughout code, and inputted hangman using "hmimage" allowing only seven letter words or less to be processed through.

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class App {
 	
 	public static void main (String []   args) {
-		String[] words = {"Banana", "Apple", "Orange", "Mango","Peach","Plum", "Strawberries", "Kiwi","Cherry"}; //Guessed Words
+		String[] words = {"Banana", "Apple", "Orange", "Mango","Peach","Plum", "Kiwi","Cherry"}; //Guessed Words
 		String word = words [(int) (Math.random() * words.length)];
 		String random = new string(new char[word.length()]).replace("\0", "*");
 		int count = 0;
@@ -13,50 +13,115 @@ public class App {
 		
 		while (count < 7 && random contains ("*")) {
 		System.out.println("Welcome, Lets Play Hangman!");
-		System.out.println("Guess A Letter!");
+		System.out.println("Guess A Letter! You Have Seven Tries.");
 		System.out.println (random);
 		String guess = sc.next();
 		hang (guess);
 		
 		}
 		sc.close();
-
-    Public static void hang(String guess) {
+		
+	}
+	
+	public static void hang(String guess) {
 		String randomtwo = "";
-		for (int i=0; i < word.length (); i++) {
+		for (int i=0; i < word.length (); i++) { //User types in a letter
 			if (word.charAt(i) == guess.charAt(0) {
 				randomtwo += guess.charAt(0);
 			} else if (random.charAt (i) != '*' {
-					randomtwo += word.charAt (i);
+					randomtwo += word.charAt (i); //Check if letter exists within the word
 			} else {
-					randomtwo += "*"
+					randomtwo += "*" //If correct, print letter within the now resolved area
 			}
-		}
-
-    if (random.equals(randomtwo)) {
+		} 
+		
+		
+		
+			
+		if (random.equals(randomtwo)) {
 			count++;
-			hmimage();
+			hmimage(); //added hangman image
 		} else {
 				random = randomtwo
 		}
 		if (random.equals(word)) {
-				System.out.println ("You've won! The word was" + word);
+				System.out.println ("You've won! The word was" + word); //finished game (won)
 			
 			}
 		
 		}
 		
+		
+		public static void hmimage() {
+			if (count == 1) {
+				System.out.println ("Wrong! Try Again!"); //If incorrect, print "Wrong! Try Again!"
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println(___);
+			}
+		
+			if (count == 2) {
+				System.out.println ("Wrong! Try Again!"); //If incorrect, print "Wrong! Try Again!"
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				System.out.println( "|" )
+				System.out.println("___");
+			}
+		
+			if (count == 3) {
+				System.out.println ("Wrong! Try Again!"); //If incorrect, print "Wrong! Try Again!"
+				System.out.println();
+				System.out.println("|");
+				System.out.println("|");
+				System.out.println("|");
+				System.out.println("___");
+			}
+		
+			if (count == 4) {
+				System.out.println ("Wrong! Try Again!"); //If incorrect, print "Wrong! Try Again!"
+				System.out.println("|");
+				System.out.println("|");
+				System.out.println("|");
+				System.out.println("___");
+			}
+		
+			if (count == 5) {
+				System.out.println ("Wrong! Try Again!"); //If incorrect, print "Wrong! Try Again!"
+				System.out.println("____________")
+				System.out.println("|");
+				System.out.println("|");
+				System.out.println("|");
+				System.out.println("|");
+				System.out.println("___");
+			}
+			if (count == 6) {
+				System.out.println ("Wrong! Try Again!"); //If incorrect, print "Wrong! Try Again!"
+				System.out.println("____________")
+				System.out.println("|           _|_");
+				System.out.println("|          /   \\");
+				System.out.println("|         |     |");
+				System.out.println("|         \\_ _/");
+				System.out.println("___          |");
+			}
+			if (count == 7) {
+				System.out.println ("Wrong! Try Again!"); //If incorrect, print "Wrong! Try Again!"
+				System.out.println("____________")
+				System.out.println("|           _|_");
+				System.out.println("|          /   \\");
+				System.out.println("|         |     |");
+				System.out.println("|         \\_ _/");
+				System.out.println("___          |");
+				System.out.println("            / \\")
+			    System.out.println("           /   \\")
+			}   //Finished Game (Lost)
 			
-		//User types in a letter
-		
-		//Check if letter exists within the word
-		
-		//If correct, print letter within the now resolved area
-		
-		//If incorrect, print "Incorrect"
+			
+			
+    }
 		
 		
-	
-
-  }
+		
 }
